8abd911ec3577de73ea22db7f3e128998a0562a1
Federico <116941799+lil-fe@users.noreply.github.com>
2023-08-29 09:43:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const MAX_NAME_LENGTH = 11;
 
 $(document).ready(function() {
     (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});
+        ({key: "NOTE", v: "weekly"});
 
     // controlla se l'utente è loggato
     checkLoginStatus();
@@ -592,7 +592,7 @@ function searchLocation(input) {
         url: `https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=${input}&lang=it_IT&units=km`,
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '81de52f504msh18b7bdd5e303a63p18528djsn997fb5d3f4b0',
+            'X-RapidAPI-Key': 'NOTE',
             'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
         }
     };
